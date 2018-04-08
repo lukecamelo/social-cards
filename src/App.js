af30060eb356d9ca19c5f0e1f047e34ffc150f62
luke <azathothbeats@gmail.com>
2018-04-08 19:41:42 -0400

    added padding to main text

--- src/App.js
+++ src/App.js
@@ -7,16 +7,37 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      bgText: ['social card', 'mic check'],
+      bgText: ['Striving every day to be a better dev.', 'mic check mic check mic check mic check mic'],
       userName: ['luke camelo', 'john egbert']
     };
   }
+
+
   render() {
+
+    // TODO implement somehow??
+    const textChangeHandler = (event, id) => {
+      const textIndex = this.state.bgText.findIndex(t => {
+        return t.id === id;
+      });
+
+      const text = {
+        ...this.state.bgText[textIndex]
+      };
+
+      text.bgText = event.target.value;
+
+      const bgText = [...this.state.bgText];
+
+      bgText[textIndex] = text;
+      this.setState({bgText: bgText})
+    }
+
     return (
       <div className="App">
         <SocialCard 
         text={this.state.bgText[0]}
-        name={this.state.userName[0]}/>
+        name={this.state.userName[0]} />
         <SocialCard 
         text={this.state.bgText[1]}
         name={this.state.userName[1]}/>
